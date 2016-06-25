551a7664e7a007c1b1af2d146c63580a1d7d1388
jvhoven <wolferine@live.se>
2016-06-25 14:45:02 +0200

    Added component tests

---
+++ test/components/category.js
@@ -0,0 +1,33 @@
+import expect from 'expect'
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import Category from '../../app/components/Category'
+
+function setup() {
+  let props = {
+    id: 0,
+    title: 'A category title',
+    style: 'sixtysix'
+  }
+
+  let renderer = TestUtils.createRenderer()
+  renderer.render(<Category {...props} />)
+  let output = renderer.getRenderOutput()
+
+  return {
+    props,
+    output,
+    renderer
+  }
+}
+
+describe('components', () => {
+  describe('Category', () => {
+    it('should render correctly', () => {
+      const { output } = setup()
+
+      expect(output.type).toBe('div')
+      expect(output.props.className).toBe('category sixtysix')
+    })
+  })
+})
---
+++ test/components/project.js
@@ -0,0 +1,33 @@
+import expect from 'expect'
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import Project from '../../app/components/Project'
+
+function setup() {
+  let props = {
+    id: 0,
+    title: 'A project title',
+    image: 'project-1.png'
+  }
+
+  let renderer = TestUtils.createRenderer()
+  renderer.render(<Project {...props} />)
+  let output = renderer.getRenderOutput()
+
+  return {
+    props,
+    output,
+    renderer
+  }
+}
+
+describe('components', () => {
+  describe('Category', () => {
+    it('should render correctly', () => {
+      const { output } = setup()
+
+      expect(output.type).toBe('div')
+      expect(output.props.className).toBe('project')
+    })
+  })
+})
