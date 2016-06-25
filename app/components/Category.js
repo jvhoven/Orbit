2085b31bd672c0c85595286cbb62890e14b5427c
jvhoven <wolferine@live.se>
2016-06-25 14:44:44 +0200

    Corrected mistake that became visible in test

--- app/components/Category.js
+++ app/components/Category.js
@@ -4,7 +4,7 @@ import ProjectList from '../containers/ProjectList'
 
 export default class Category extends Component {
   render() {
-    const { id, projects, style, title } = this.props
+    const { id, style, title } = this.props
     let classes = classNames('category', style)
 
     return (
