8f181beb9ad0ab97d625c6644676d29a3b47d3d6
jvhoven <jeffreyvanhoven@gmail.com>
2016-06-28 21:10:59 +0200

    In progress code for going to an existing project' page

--- src/js/components/Project.js
+++ src/js/components/Project.js
@@ -1,11 +1,22 @@
 import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { push } from 'react-router-redux'
+import { connect } from 'react-redux'
 
-export default class Project extends Component {
+export class Project extends Component {
+  /*
+  * TODO: Implement correct route
+  */
+  gotoProject (e, id) {
+    const { push } = this.props
+    push('/project')
+  }
+
   render () {
     const { image, title } = this.props
 
     return (
-      <div className='project'>
+      <div onClick={(e, id) => this.gotoProject(e, this.props.id)} className='project'>
         <img src={image} alt=''/>
         <span>
           {title}
@@ -16,6 +27,13 @@ export default class Project extends Component {
 }
 
 Project.propTypes = {
+  id: PropTypes.number.isRequired,
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired
 }
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({ push }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(Project)
